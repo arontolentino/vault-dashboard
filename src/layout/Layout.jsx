f511d593e122ebd802ded94d2d011fc84c44307b
Aron Tolentino <arontolentino@outlook.com>
2022-01-10 11:05:21 -0500

    Fix main section height

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -31,7 +31,7 @@ const userNavigation = [
 export default function Layout({ children }) {
   return (
     <>
-      <div className="h-screen bg-gray-50 flex flex-col">
+      <div className="h-screen flex flex-col">
         <Disclosure as="nav" className="bg-black">
           {({ open }) => (
             <>
@@ -184,7 +184,7 @@ export default function Layout({ children }) {
           )}
         </Disclosure>
 
-        <main className="flex-grow">
+        <main className="flex-grow bg-gray-50 ">
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             {children}
           </div>
